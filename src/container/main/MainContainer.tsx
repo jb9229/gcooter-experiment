963621306c6db0824b4c665a7badc4e5c41446aa
gon <gonmac@Allenchoisui-MacBook-Pro.local>
2021-05-28 11:27:29 +0900

    lint: apply eslint configuration

--- src/container/main/MainContainer.tsx
+++ src/container/main/MainContainer.tsx
@@ -5,21 +5,34 @@ import MainLayout from './MainLayout';
 import { vehiclesOfMap } from './store';
 
 const MainContainer: React.FC = () => {
-    //states
-    const setVehiclesOfMap = useSetRecoilState(vehiclesOfMap);
-    const rspOfVehicleList = (rsp: VehicleResponse) => {
-        console.log('success vehicle list call: ', rsp.data.length);
-        // console.log('success vehicle list call: ', data);
-        const vehicles = rsp.data;
-        setVehiclesOfMap(rsp.data);
-    }
-    const failedVehicleList = () => {
-        console.log('failed vehicle list call!!')
-    }
+  //states
+  const setVehiclesOfMap = useSetRecoilState(vehiclesOfMap);
 
-    const location = { latitude: 37.62353703396386, longitude: 127.06104452210563 };
-    const result = GET_VEHICLE_LIST_IN_BOUND({location: location, successed: rspOfVehicleList, failed: failedVehicleList});
-    return <MainLayout/>;
-}
+  const rspOfVehicleList = (rsp: VehicleResponse) => {
+    console.log('success vehicle list call: ', rsp.data.length);
+
+    // console.log('success vehicle list call: ', data);
+    const vehicles = rsp.data;
+
+    setVehiclesOfMap(rsp.data);
+  };
+
+  const failedVehicleList = () => {
+    console.log('failed vehicle list call!!');
+  };
+
+  const location = {
+    latitude: 37.62353703396386,
+    longitude: 127.06104452210563,
+  };
+
+  const result = GET_VEHICLE_LIST_IN_BOUND({
+    location: location,
+    successed: rspOfVehicleList,
+    failed: failedVehicleList,
+  });
+
+  return <MainLayout />;
+};
 
 export default MainContainer;
--- src/container/main/MainLayout.tsx
+++ src/container/main/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import GcooterMarker from '../../components/svg/GcooterMarker';
 import { useRecoilValue } from 'recoil';
 import { vehiclesOfMap } from './store';
@@ -15,33 +15,64 @@ const MainLayout: React.FC = () => {
   const windowHeight = Dimensions.get('window').height;
   const ASPECT_RATIO = windowWidth / windowHeight;
   const vehicles = useRecoilValue(vehiclesOfMap);
+
   const initialRegion = {
     latitude: 37.62353703396386,
     longitude: 127.06104452210563,
     latitudeDelta: 0.0112,
-    longitudeDelta: 0.0112 * ASPECT_RATIO
+    longitudeDelta: 0.0112 * ASPECT_RATIO,
   };
 
-  const markers = vehicles.map((vehicle) => ({id: vehicle.id, latlng: {latitude: Number.parseFloat(vehicle.lat), longitude: Number.parseFloat(vehicle.lng)}, title: vehicle.title, description: vehicle.description, battery: vehicle.battery}));
+  const markers = vehicles.map((vehicle) => ({
+    id: vehicle.id,
+    latlng: {
+      latitude: Number.parseFloat(vehicle.lat),
+      longitude: Number.parseFloat(vehicle.lng),
+    },
+    title: vehicle.title,
+    description: vehicle.description,
+    battery: vehicle.battery,
+  }));
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} showsUserLocation={true} initialRegion={initialRegion}>
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        initialRegion={initialRegion}
+      >
         {markers.map((marker, index) => (
-          <MarkerWrap key={marker.id} onPress={() => setSelectedMarker(marker.id)}>
+          <MarkerWrap
+            key={marker.id}
+            onPress={() => setSelectedMarker(marker.id)}
+          >
             <Marker
               coordinate={marker.latlng}
               title={marker.title}
               description={marker.description}
             >
-              {selectedMarker === marker.id ? (<GcooterMarker size={40} fill="#000000" backgroundColor="#072c1a" selected={true} battery={marker.battery} />) : (<GcooterMarker size={40} fill="#000000" battery={marker.battery} />)}
+              {selectedMarker === marker.id ? (
+                <GcooterMarker
+                  size={40}
+                  fill="#000000"
+                  backgroundColor="#072c1a"
+                  selected={true}
+                  battery={marker.battery}
+                />
+              ) : (
+                <GcooterMarker
+                  size={40}
+                  fill="#000000"
+                  battery={marker.battery}
+                />
+              )}
             </Marker>
           </MarkerWrap>
         ))}
       </MapView>
     </View>
   );
-}
+};
 
 export default MainLayout;
 
